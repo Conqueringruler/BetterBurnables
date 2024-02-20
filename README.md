c9392001bf95424709774c65bb75dabc3bf62dc1
A L <abemlong@gmail.com>
2024-02-20 14:16:35 -0800

    v 1.0 made

--- README.md
+++ README.md
@@ -1,37 +1,38 @@
-# Example Mod
+# Better Burnables
 
-Template for making Babric mods for BTA!
+One of the things I was really surprised about when I first played BTA was how few items could be used in furnaces, 
+especially with some that realistically should be burnable. If there's anything you think I missed, let me know!
 
-**Note: *DO NOT fork this repository unless you want to contribute!***
-
-## Prerequisites
-- JDK for Java 17 ([Eclipse Temurin](https://adoptium.net/temurin/releases/) recommended)
-- [Intellij IDEA](https://www.jetbrains.com/idea/download/) (Scroll down for the free community edition, if using linux **DO NOT** use the flatpak distribution)
-- Minecraft Development plugin (Optional, but highly recommended)
-
-## Setup instructions
-   
-
-1. Click the `Use this template` button on this repo's page above. Choose `Create a new repository`, you will be redirected to a new page. Enter your repo's name and description, and hit `Create repository`.  
-   To get your project, open IntelliJ IDEA and click `Get from VCS`. Select `Repository URL` and enter your repo's url
-
-2. After the project has finished importing, close it and open it again.  
-   If that does not work, open the right sidebar with `Gradle` on it, open `Tasks` > `fabric` and run `ideaSyncTask`.
-
-3. Create a new run configuration by going in `Run > Edit Configurations`.  
-   Then click on the plus icon and select Gradle. In the `Tasks and Arguments` field enter `build`.  
-   Running it will build your finished jar files and put them in `build/libs/`.
-
-4. Lastly, open `File` > `Settings` and head to `Build, Execution, Development` > `Build Tools` > `Gradle`.  
-   Make sure `Build and run using` and `Run tests using` is set to `Gradle`.
-
-5. Done! Now, all that's left is to change every mention of `examplemod` and `turniplabs` to your own mod id and mod group, respectively. Happy modding!
-
-## Tips
-
-1. If you haven't already you should join the BTA modding discord! https://discord.gg/FTUNJhswBT
-2. You can set your username when launching the client run configuration by setting `--username <username>` in your program arguments.
-3. When launching the server run configuration you may want to remove the `nogui` program argument in order to see the regular server GUI.
-4. In Intellij you can double press shift or press ctrl+N to search class files, change the search from the default `Project Files` to `All Places` you can easily explore the classes for you dependencies and even BTA itself.
-5. In Intellij if ctrl+left click on a field or method you can quickly get information on when and where that field or method is assign or used.
-
+## Tons of items now burnable!
+As of v. 1.0 the full list is:
+- Fallen Leaves
+- Cobweb
+- Wheat Seeds
+- Pumpkin Seeds
+- Cacao Saplings (the devs forgot about this one lmao)
+- Paper
+- Wheat
+- String
+- Ferns
+- Tall Grass
+- All Leaves (Oak, Birch, Cacao, Cherry & Cherry Flowering, Pine, Retro, Shrub, Eucalyptus)
+- Arrows
+- Torches
+- Cloth
+- All Wool (so like 9 items)
+- Labels
+- Baked Sugarcane Blocks
+- Books
+- Flags
+- Paintings
+- Ladders
+- Maps
+- Bookshelves
+- Seats
+- Baskets
+- Seats again (why two?)
+- Wooden Trapdoor
+- Carved Pumpkin
+- Jack O' Lantern
+- Paper Fence
+- Paper Wall
---
+++ src/main/java/conq_/betterburnables/mixin/FurnaceMixin.java
@@ -0,0 +1,104 @@
+package conq_.betterburnables.mixin;
+import net.minecraft.core.block.BlockStairs;
+import net.minecraft.core.crafting.LookupFuelFurnace;
+import net.minecraft.core.enums.EnumDropCause;
+import net.minecraft.core.item.ItemSeeds;
+import net.minecraft.core.item.ItemStack;
+import net.minecraft.core.world.World;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import net.minecraft.core.block.Block;
+import net.minecraft.core.block.BlockLeavesBase;
+import net.minecraft.core.block.entity.TileEntity;
+import net.minecraft.core.enums.EnumDropCause;
+import net.minecraft.core.item.Item;
+import net.minecraft.core.item.ItemStack;
+import net.minecraft.core.world.World;
+import net.minecraft.core.block.BlockStairs;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import turniplabs.halplibe.helper.recipeBuilders.RecipeBuilderFurnace;
+import turniplabs.halplibe.helper.recipeBuilders.RecipeBuilderBlastFurnace;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import java.util.HashMap;
+import java.util.Map;
+import net.minecraft.core.block.Block;
+import net.minecraft.core.item.Item;
+
+
+@Mixin(value = {LookupFuelFurnace.class}, remap = false)
+
+public class FurnaceMixin {
+
+@Shadow @Final
+protected Map<Integer, Integer> fuelList;
+	@Shadow public void addFuelEntry(int id, int fuelYield) {
+		this.fuelList.put(id, fuelYield);
+	}
+	@Inject(method = "register", at = @At(value = "TAIL"))
+	public void register(CallbackInfo ci)
+	{
+		this.addFuelEntry(Block.layerLeavesOak.id, 10);
+		this.addFuelEntry(Block.cobweb.id, 10);
+		this.addFuelEntry(Item.seedsWheat.id, 10);
+		this.addFuelEntry(Item.seedsPumpkin.id, 10);
+		this.addFuelEntry(Block.saplingCacao.id, 10);
+		this.addFuelEntry(Item.paper.id, 10);
+		this.addFuelEntry(Item.wheat.id, 10);
+		this.addFuelEntry(Item.string.id, 10);
+		this.addFuelEntry(Block.tallgrass.id, 10);
+		this.addFuelEntry(Block.tallgrassFern.id, 10);
+
+		this.addFuelEntry(Block.leavesBirch.id, 10);
+		this.addFuelEntry(Block.leavesCacao.id, 10);
+		this.addFuelEntry(Block.leavesCherry.id, 10);
+		this.addFuelEntry(Block.leavesOak.id, 10);
+		this.addFuelEntry(Block.leavesCherryFlowering.id, 20);
+		this.addFuelEntry(Block.leavesPine.id, 10);
+		this.addFuelEntry(Block.leavesOakRetro.id, 10);
+		this.addFuelEntry(Block.leavesShrub.id, 10);
+		this.addFuelEntry(Block.leavesEucalyptus.id, 10);
+
+		this.addFuelEntry(Item.ammoArrow.id, 20);
+		this.addFuelEntry(Block.torchCoal.id, 20);
+		this.addFuelEntry(Item.cloth.id, 20);
+
+		this.addFuelEntry(Block.wool.id, 50);
+		this.addFuelEntry(Item.label.id, 50);
+		this.addFuelEntry(Block.blockSugarcaneBaked.id, 50);
+		this.addFuelEntry(Item.book.id, 50);
+
+		this.addFuelEntry(Item.flag.id, 150);
+		this.addFuelEntry(Item.painting.id, 150);
+		this.addFuelEntry(Block.ladderOak.id, 150);
+		this.addFuelEntry(Item.map.id, 150);
+
+
+		this.addFuelEntry(Block.bookshelfPlanksOak.id, 300);
+		this.addFuelEntry(Block.seat.id, 300);
+		this.addFuelEntry(Block.basket.id, 300);
+		this.addFuelEntry(Item.seat.id, 300);
+		this.addFuelEntry(Block.trapdoorPlanksOak.id, 300);
+		this.addFuelEntry(Block.pumpkinCarvedIdle.id, 300);
+		this.addFuelEntry(Block.fencePaperWall.id, 300);
+
+		this.addFuelEntry(Block.pumpkinCarvedActive.id, 500);
+		this.addFuelEntry(Block.paperWall.id, 500);
+
+
+	}
+	//@Shadow private Block modelBlock;
+	/**
+	@Inject(method = "getBreakResult", at = @At("TAIL"),remap = false, cancellable = true)
+	private void stairs(World world, EnumDropCause dropCause, int x, int y, int z, int meta, TileEntity tileEntity, CallbackInfoReturnable<ItemStack[]> cir) {
+		if(world.rand.nextFloat()<0.005){ // 0.001 Should be 0.1% chance aka 1/1000. 0.005 = 1/200 chance.
+			cir.setReturnValue(new ItemStack[]{new ItemStack(StairsDisc.stairsDisc, 1)});
+
+		}
+	} */
+
+}
