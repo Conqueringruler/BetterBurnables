8e379e2752d04f30c4da06cd7d3e905536d73f94
A L <abemlong@gmail.com>
2024-02-20 15:58:02 -0800

    v 1.0 made

--- src/main/java/turniplabs/examplemod/BetterBurnables.java
+++ src/main/java/turniplabs/examplemod/BetterBurnables.java
@@ -4,12 +4,12 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
+public class BetterBurnables implements ModInitializer {
     public static final String MOD_ID = "examplemod";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
-        LOGGER.info("ExampleMod initialized.");
+        LOGGER.info("BetterBurnables initialized.");
     }
 }
